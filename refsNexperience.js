579efee0805c6aefd84c67480c5a8ba4c84a72c8
Joseph Panetta <jlpanetta1681@icloud.com>
2021-09-20 23:54:07 -0400

    starteed refernces component

---
+++ refsNexperience.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function refsNexperience() {
+    return (
+        <div>
+            Professional as well as personal references and their info can be found here"
+        </div>
+    )
+}
+
+export default refsNexperience
